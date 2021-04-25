bd535f256b4fb377e056f27c676076a32f07aa52
Joseph Afriyie Attakorah <juxluvjoe@gmail.com>
2021-04-25 18:27:18 +0300

    fixed bug and added comments

--- eo-runtime/src/main/java/org/eolang/EOjava.java
+++ eo-runtime/src/main/java/org/eolang/EOjava.java
@@ -1,46 +1,82 @@
 package org.eolang;
 
 import org.eolang.core.EOObject;
-import org.eolang.core.data.EOData;
+import org.eolang.core.data.EODataObject;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+
+/**
+ * Wrapper for Java classes
+ * Turns a java class instance or method into an EOObject
+ */
 public class EOjava extends EOObject {
 
+    /**
+     * creates a new java class instance and turns it into an {@code EOObject}
+     * @param javaClass a EO string object representing the specific java class to instantiate
+     * @return An {@code EOObject} resenting the Java class instance
+     */
     public EOObject EOnew(EOObject javaClass){
-        EOstring result = new EOstring();
+        EOObject result = new EOstring();
         try{
             Class<?> cls = Class.forName(javaClass._getData().toString());
             Constructor<?> constructor = cls.getConstructor();
-            System.out.println("Print instance: " + constructor.newInstance());
-            result = new EOstring((String) constructor.newInstance());
+            result = new EODataObject(constructor.newInstance().toString());
             return result;
-        } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | IllegalAccessException | InstantiationException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return result;
     }
 
-    public EOObject EOget(EOObject javaClass, EOstring methodName){
-        EOstring result = new EOstring();
-        System.out.println("Trying...");
-        try{
+    /**
+     * Calls an instance method and turns it into an {@code EOObject}
+     * @param javaClass an EO string object representing the specific java class whose method is to be executed
+     * @param methodName an EO string object representing the specific java method to execute
+     * @param arguments EO string object(s) representing the arguments to pass to the java method
+     * @return an {@code EOObject} representing the results of executing the java method
+     */
+    public EOObject EOexec(EOObject javaClass, EOObject methodName, EOObject... arguments) {
+        EOObject result = new EOstring();
+        try {
             Class<?> cls = Class.forName(javaClass._getData().toString());
-            Method method = cls.getDeclaredMethod(methodName._getData().toString());
+            Method method = Arrays.stream(cls.getMethods())
+                    .filter(_method -> _method.getName().equals(
+                            methodName._getData().toString())
+                    ).findFirst().get();
+            Parameter[] methodParams = method.getParameters();
             method.setAccessible(true);
-            System.out.println("Print method: " + method.invoke(cls));
-            result = new EOstring((String) method.invoke(cls));
-            return result;
-        } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            result = new EODataObject(
+                    method.invoke(
+                            cls,
+                            _prepareArgs(methodParams, arguments)
+                    ).toString());
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return result;
+        return  result;
     }
 
-    @Override
-    public EOData _getData() {
-        return super._getData();
+    private Object[] _prepareArgs(Parameter[] methodParams, EOObject... arguments) {
+        List<Object> methodValues = new ArrayList<>();
+        for (int i = 0; i < methodParams.length; i++) {
+            if (methodParams[i].getType().getCanonicalName().endsWith("[]")) {
+                methodValues.add(
+                        Arrays
+                        .stream(arguments)
+                        .skip(i)
+                        .toArray(EOObject[]::new));
+                break;
+            } else {
+                methodValues.add(arguments[i]);
+            }
+        }
+        return methodValues.toArray();
     }
 }
