466b0beea3d8cfc5a3788b57b9270312a1a6b002
Joseph Afriyie Attakorah <juxluvjoe@gmail.com>
2021-04-25 15:01:10 +0300

    updated Java wrapper

--- eo-runtime/src/main/java/org/eolang/EOjava.java
+++ eo-runtime/src/main/java/org/eolang/EOjava.java
@@ -5,6 +5,7 @@ import org.eolang.core.data.EOData;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class EOjava extends EOObject {
 
@@ -17,6 +18,22 @@ public class EOjava extends EOObject {
             result = new EOstring((String) constructor.newInstance());
             return result;
         } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | IllegalAccessException | InstantiationException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public EOObject EOget(EOObject javaClass, EOstring methodName){
+        EOstring result = new EOstring();
+        System.out.println("Trying...");
+        try{
+            Class<?> cls = Class.forName(javaClass._getData().toString());
+            Method method = cls.getDeclaredMethod(methodName._getData().toString());
+            method.setAccessible(true);
+            System.out.println("Print method: " + method.invoke(cls));
+            result = new EOstring((String) method.invoke(cls));
+            return result;
+        } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
             e.printStackTrace();
         }
         return result;
