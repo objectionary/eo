e426850ba658d1d59e07a3d9ca6d6cd4f6395ee4
aventador3000 <50798505+aventador3000@users.noreply.github.com>
2021-05-18 17:25:58 +0300

    Added indexreduce,complete 1st version of Lcom1 metric using EO,the stage of testing and debugging (#11)
    Co-authored-by: aventador 3000 <vkorzun2000>

--- eo-runtime/src/main/java/org/eolang/EOarray.java
+++ eo-runtime/src/main/java/org/eolang/EOarray.java
@@ -82,15 +82,30 @@ public class EOarray extends EOObject {
     /***
      * Performs the couple reduction operation of the base array object
      * @param accumulator a partial/subtotal result
-     * @param reduceFunction represents the reduction function
+     * @param couplereduceFunction represents the reduction function
      * @return An object representing the final accumulated value of the coupled reduce operation
      */
-    public EOObject EOcouplereduce(EOObject accumulator, EOObject reduceFunction) {
+    public EOObject EOcouplereduce(EOObject accumulator, EOObject couplereduceFunction) {
         EOObject out = accumulator;
         for (EOObject eoObject1 : this._array) {
             for(EOObject eoObject2 : this._array){
-            out = reduceFunction._getAttribute("EOcouplereduce", out, eoObject1,eoObject2)._getDecoratedObject();
+            out = couplereduceFunction._getAttribute("EOcouplereduce", out, eoObject1,eoObject2)._getDecoratedObject();
         }}
+        return out;
+    }
+
+    /***
+     * Performs the reduction operation of the base array object
+     * @param accumulator a partial/subtotal result
+     * @param reduceiFunction represents the reduction function
+     * @return An object representing the final accumulated value of the reduce operation
+     */
+    public EOObject EOreducei(EOObject accumulator, EOObject reduceiFunction) {
+        EOObject out = accumulator;
+        int length =_array.size();
+        for(int i = 0; i < length;i++){
+            out = reduceiFunction._getAttribute("EOreducei", out,_array.get(i),new EODataObject(i))._getDecoratedObject();
+            }
         return out;
     }
 
