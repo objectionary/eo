62ef76c489942d19b47b390bbd4dc6d0a25dfd06
Yegor Bugayenko <yegor256@gmail.com>
2021-11-19 13:11:29 +0300

    #436 typo

--- eo-runtime/src/main/java/org/eolang/Dataized.java
+++ eo-runtime/src/main/java/org/eolang/Dataized.java
@@ -90,11 +90,10 @@ public final class Dataized {
         Dataized.LOGGER.log(
             Level.FINE,
             String.format(
-                "%s\uD835\uDD3B( %s ) ➜ %s (%s)",
+                "%s\uD835\uDD3B( %s ) ➜ %s",
                 String.join("", Collections.nCopies(before, "·")),
                 this.phi.φTerm(),
-                new Data.Value<>(data).φTerm(),
-                this.phi.getClass().getCanonicalName()
+                new Data.Value<>(data).φTerm()
             )
         );
         Dataized.LEVEL.set(before);
