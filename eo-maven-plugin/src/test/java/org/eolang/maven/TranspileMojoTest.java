403da8ad76cef570ce639da55433c85a519cbec7
Olivier B. OURA <baudoliver7@gmail.com>
2021-12-21 23:17:31 +0000

    deps: use array.eo directly from eo-runtime
    Both windows and linux support this.
    
    Ref: #465

--- eo-maven-plugin/src/test/java/org/eolang/maven/TranspileMojoTest.java
+++ eo-maven-plugin/src/test/java/org/eolang/maven/TranspileMojoTest.java
@@ -26,7 +26,7 @@ package org.eolang.maven;
 import com.jcabi.log.Logger;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import org.apache.commons.lang3.SystemUtils;
+import java.nio.file.Paths;
 import org.apache.maven.plugin.testing.stubs.MavenProjectStub;
 import org.cactoos.Input;
 import org.cactoos.io.InputOf;
@@ -121,15 +121,11 @@ public final class TranspileMojoTest {
     @Test
     public void testRealCompilation(@TempDir final Path temp)
         throws Exception {
-        final String arpath;
-        if (SystemUtils.IS_OS_WINDOWS) {
-            arpath = "org/eolang/maven/win-array.eo";
-        } else {
-            arpath = "org/eolang/maven/array.eo";
-        }
         final String java = this.compile(
             temp,
-            new ResourceOf(arpath),
+            new InputOf(
+                Paths.get("../eo-runtime/src/main/eo/org/eolang/array.eo")
+            ),
             "EOorg/EOeolang/EOarray.java"
         );
         MatcherAssert.assertThat(java, Matchers.containsString("class"));
