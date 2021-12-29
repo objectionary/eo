d4570d4422a3cc193dd66832bb6a98c99fce9234
andreoss <andreoss@sdf.org>
2021-12-28 23:20:36 -0500

    (#490) - Implement LocalObjectionary & FallbackObjectionary

--- eo-maven-plugin/src/main/java/org/eolang/maven/CachingObjectionary.java
+++ eo-maven-plugin/src/main/java/org/eolang/maven/CachingObjectionary.java
@@ -23,75 +23,57 @@
  */
 package org.eolang.maven;
 
-import com.jcabi.log.Logger;
 import java.io.IOException;
-import java.net.URL;
+import java.nio.file.Path;
 import org.cactoos.Input;
-import org.cactoos.io.InputOf;
+import org.cactoos.io.TeeInput;
 
 /**
- * The simple HTTP Objectionary server.
+ * Objectionary which caches objects locally.
  *
- * @since 0.1
+ * @since 1.0
  */
-public final class RemoteObjectionary implements Objectionary {
+public final class CachingObjectionary implements Objectionary {
 
     /**
-     * The hash code to fetch.
+     * Cache location.
      */
-    private final String hash;
+    private final Path cache;
 
     /**
-     * The address template.
+     * Version or hash.
      */
-    private final String addr;
+    private final String version;
 
     /**
-     * Ctor.
+     * Remote Objectionary.
      */
-    public RemoteObjectionary() {
-        this("master");
-    }
-
-    /**
-     * Ctor.
-     * @param hsh The GitHub hash
-     * @todo #490:30m Resolve abbreviated hash to a proper hash.
-     *  In order to avoid collisions resolve hash
-     *  (or branch) to a complete sha-256 hash of the commit.
-     *  Use only sha-256 hashes as a caching criteria.
-     */
-    public RemoteObjectionary(final String hsh) {
-        this(
-            // @checkstyle LineLength (1 line)
-            "https://raw.githubusercontent.com/yegor256/objectionary/%s/objects/%s.eo",
-            hsh
-        );
-    }
+    private final Objectionary primary;
 
     /**
      * Ctor.
-     * @param url The url template.
-     * @param hsh The hash or branch.
+     * @param ver Version.
+     * @param cache Cache directory.
+     * @param primary Primary objectionary.
      */
-    public RemoteObjectionary(final String url, final String hsh) {
-        this.addr = url;
-        this.hash = hsh;
+    public CachingObjectionary(
+        final String ver,
+        final Path cache,
+        final Objectionary primary
+    ) {
+        this.version = ver;
+        this.cache = cache;
+        this.primary = primary;
     }
 
     @Override
     public Input get(final String name) throws IOException {
-        final URL url = new URL(
-            String.format(
-                this.addr,
-                this.hash, name.replace(".", "/")
-            )
+        return new TeeInput(
+            this.primary.get(name),
+            this.cache
+                .resolve("sources")
+                .resolve(this.version)
+                .resolve(String.format("%s.eo", name.replace(".", "/")))
         );
-        Logger.debug(
-            this, "The object '%s' will be pulled from %s...",
-            name, url
-        );
-        return new InputOf(url);
     }
-
 }
