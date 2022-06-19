9ce38ab3ebae2f077b79224f33f9e406d8b7794b
Yegor Bugayenko <yegor256@gmail.com>
2022-06-19 16:51:38 +0300

    #762 better log

--- eo-maven-plugin/src/main/java/org/eolang/maven/TranspileMojo.java
+++ eo-maven-plugin/src/main/java/org/eolang/maven/TranspileMojo.java
@@ -114,7 +114,7 @@ public final class TranspileMojo extends SafeMojo {
                 this.generatedDir.getAbsolutePath()
             );
             Logger.info(
-                this, "The directory added to compile-source-root: %s",
+                this, "The directory added to Maven 'compile-source-root': %s",
                 Save.rel(this.generatedDir.toPath())
             );
         }
@@ -123,7 +123,7 @@ public final class TranspileMojo extends SafeMojo {
                 this.generatedDir.getAbsolutePath()
             );
             Logger.info(
-                this, "The directory added to test-compile-source-root: %s",
+                this, "The directory added to Maven 'test-compile-source-root': %s",
                 Save.rel(this.generatedDir.toPath())
             );
         }
