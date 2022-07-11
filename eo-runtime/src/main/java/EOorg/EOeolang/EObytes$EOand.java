d71543a604aa59d8d9e07818e7e6c90f8951bb3e
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-07-11 20:54:07 +0500

    Update EObytes$EOand.java
    Make it work

--- eo-runtime/src/main/java/EOorg/EOeolang/EObytes$EOand.java
+++ eo-runtime/src/main/java/EOorg/EOeolang/EObytes$EOand.java
@@ -26,8 +26,10 @@ package EOorg.EOeolang;
 
 import java.math.BigInteger;
 import org.eolang.AtComposite;
+import org.eolang.AtFree;
 import org.eolang.AtVararg;
 import org.eolang.Data;
+import org.eolang.Dataized;
 import org.eolang.Param;
 import org.eolang.PhDefault;
 import org.eolang.Phi;
@@ -55,8 +57,17 @@ public class EObytes$EOand extends PhDefault {
                 rho -> {
                     BigInteger base = new Param(rho).fromBytes(BigInteger.class);
                     final Phi[] args = new Param(rho, "b").strong(Phi[].class);
-                    for (int index = 0; index < args.length; index++) {
-                        base = base.and(new Param(args[index]).fromBytes(BigInteger.class));
+                    for (int index = 0; index < args.length; ++index) {
+                        final Object val = new Dataized(args[index]).take();
+                        if (!(val instanceof byte[])) {
+                            throw new IllegalArgumentException(
+                                String.format(
+                                    "The %dth argument of 'and' is of type %s, not bytes",
+                                    index, val.getClass().getCanonicalName()
+                                )
+                            );
+                        }
+                        base = base.and(new BigInteger(byte[].class.cast(val)));
                     }
                     return new Data.ToPhi(base.toByteArray());
                 }
