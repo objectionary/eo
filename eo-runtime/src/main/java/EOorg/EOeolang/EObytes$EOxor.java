6688e560e5d0f31bdb350d5eec1d429338d77fde
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-07-12 21:14:01 +0500

    Make `xor` accept vararg

--- eo-runtime/src/main/java/EOorg/EOeolang/EObytes$EOxor.java
+++ eo-runtime/src/main/java/EOorg/EOeolang/EObytes$EOxor.java
@@ -29,8 +29,9 @@ package EOorg.EOeolang;
 
 import java.math.BigInteger;
 import org.eolang.AtComposite;
-import org.eolang.AtFree;
+import org.eolang.AtVararg;
 import org.eolang.Data;
+import org.eolang.Dataized;
 import org.eolang.Param;
 import org.eolang.PhDefault;
 import org.eolang.Phi;
@@ -51,16 +52,28 @@ public class EObytes$EOxor extends PhDefault {
      */
     public EObytes$EOxor(final Phi sigma) {
         super(sigma);
-        this.add("b", new AtFree());
+        this.add("b", new AtVararg());
         this.add(
             "φ",
             new AtComposite(
                 this,
-                rho -> new Data.ToPhi(
-                    new Param(rho).fromBytes(BigInteger.class)
-                        .xor(new Param(rho, "b").fromBytes(BigInteger.class))
-                        .toByteArray()
-                )
+                rho -> {
+                    BigInteger base = new Param(rho).fromBytes(BigInteger.class);
+                    final Phi[] args = new Param(rho, "b").strong(Phi[].class);
+                    for (int index = 0; index < args.length; ++index) {
+                        final Object val = new Dataized(args[index]).take();
+                        if (!(val instanceof byte[])) {
+                            throw new IllegalArgumentException(
+                                String.format(
+                                    "The %dth argument of 'and' is of type %s, not bytes",
+                                    index, val.getClass().getCanonicalName()
+                                )
+                            );
+                        }
+                        base = base.xor(new BigInteger(byte[].class.cast(val)));
+                    }
+                    return new Data.ToPhi(base.toByteArray());
+                }
             )
         );
     }
