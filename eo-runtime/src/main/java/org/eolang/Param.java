cf4fd15407e46c61b81341b85342e594119afd72
maximtrunnikov <m.trunnikov@mail.ru>
2022-07-30 14:38:30 +0300

    try to find x x5

--- eo-runtime/src/main/java/org/eolang/Param.java
+++ eo-runtime/src/main/java/org/eolang/Param.java
@@ -84,15 +84,15 @@ public final class Param {
         if (!type.isInstance(ret)) {
             throw new ExFailure(
                 String.format(
-                    "The argument '.%s' with value %s (rho %s, phi %s) is of Java type '%s', not '%s' as expected",
+                    "The argument '.%s' with value %s (rho %s, phi term %s, loc %s, to str %s) is of Java type '%s', not '%s' as expected",
                     this.attr,
                     ret,
                     new Dataized(
                         this.rho.attr("ρ").get()
                     ).take(),
-                    new Dataized(
-                        this.rho.attr("φ").get()
-                    ).take(),
+                    this.rho.φTerm(),
+                    this.rho.location(),
+                    this.rho.toString(),
                     ret.getClass().getCanonicalName(),
                     type.getCanonicalName()
                 )
