219b7769123a0a86ee1822cb55d6eb876a204ce6
OlesiaSub <lesya.sub@mail.ru>
2022-08-18 13:22:10 +0300

    Style fixes

--- eo-maven-plugin/src/test/java/org/eolang/maven/SkipTest.java
+++ eo-maven-plugin/src/test/java/org/eolang/maven/SkipTest.java
@@ -7,20 +7,21 @@ import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 /**
  * Test case for skip option in {@link SafeMojo}.
+ *
+ * @since 0.22
  */
 public class SkipTest {
 
     @Test
     public void testExecutedPullMojo(@TempDir final Path temp) {
         final Path target = temp.resolve("target");
-        executePullMojo(temp, target, false);
+        this.executePullMojo(temp, target, false);
         MatcherAssert.assertThat(
             Files.exists(
                 target.resolve(
@@ -37,7 +38,7 @@ public class SkipTest {
     @Test
     public void testSkippedPullMojo(@TempDir final Path temp) {
         final Path target = temp.resolve("target");
-        executePullMojo(temp, target, true);
+        this.executePullMojo(temp, target, true);
         MatcherAssert.assertThat(
             !Files.exists(
                 target.resolve(
@@ -51,7 +52,33 @@ public class SkipTest {
         );
     }
 
-    private void executePullMojo(@TempDir final Path temp, final Path target, final boolean skip) {
+    @Test
+    public void testSkippedCopyMojo(@TempDir final Path temp) throws IOException {
+        final Path classes = temp.resolve("classes");
+        this.executeCopyMojo(temp, classes, true);
+        final Path out = classes.resolve("EO-SOURCES/foo/main.eo");
+        MatcherAssert.assertThat(
+                !Files.exists(out),
+                Matchers.is(true)
+        );
+    }
+
+    @Test
+    public void testExecutedCopyMojo(@TempDir final Path temp) throws IOException {
+        final Path classes = temp.resolve("classes");
+        this.executeCopyMojo(temp, classes, false);
+        final Path out = classes.resolve("EO-SOURCES/foo/main.eo");
+        MatcherAssert.assertThat(
+                Files.exists(out),
+                Matchers.is(true)
+        );
+    }
+
+    private void executePullMojo(
+            @TempDir final Path temp,
+            final Path target,
+            final boolean skip
+    ) {
         final Path foreign = temp.resolve("eo-foreign.json");
         new MonoTojos(new Json(foreign))
             .add("org.eolang.io.stdout")
@@ -69,29 +96,11 @@ public class SkipTest {
             .execute();
     }
 
-    @Test
-    public void testSkippedCopyMojo(@TempDir final Path temp) throws IOException {
-        final Path classes = temp.resolve("classes");
-        executeCopyMojo(temp, classes, true);
-        final Path out = classes.resolve("EO-SOURCES/foo/main.eo");
-        MatcherAssert.assertThat(
-            !Files.exists(out),
-            Matchers.is(true)
-        );
-    }
-
-    @Test
-    public void testExecutedCopyMojo(@TempDir final Path temp) throws IOException {
-        final Path classes = temp.resolve("classes");
-        executeCopyMojo(temp, classes, false);
-        final Path out = classes.resolve("EO-SOURCES/foo/main.eo");
-        MatcherAssert.assertThat(
-            Files.exists(out),
-            Matchers.is(true)
-        );
-    }
-
-    private void executeCopyMojo(@TempDir final Path temp, final Path classes, final boolean skip) throws IOException {
+    private void executeCopyMojo(
+            @TempDir final Path temp,
+            final Path classes,
+            final boolean skip
+    ) throws IOException {
         final Path src = temp.resolve("src");
         new Save(
             "+rt foo:0.0.0\n\n[args] > main\n  \"0.0.0\" > @\n",
