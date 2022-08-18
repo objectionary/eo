b87c81ee9cd5fbc222a9b6b487337a7b07bfb662
OlesiaSub <lesya.sub@mail.ru>
2022-08-18 13:28:17 +0300

    Style fixes

--- eo-maven-plugin/src/test/java/org/eolang/maven/SkipTest.java
+++ eo-maven-plugin/src/test/java/org/eolang/maven/SkipTest.java
@@ -1,15 +1,39 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2022 Objectionary.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package org.eolang.maven;
 
 import com.yegor256.tojos.Json;
 import com.yegor256.tojos.MonoTojos;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import org.cactoos.io.InputOf;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 /**
  * Test case for skip option in {@link SafeMojo}.
@@ -58,8 +82,8 @@ public class SkipTest {
         this.executeCopyMojo(temp, classes, true);
         final Path out = classes.resolve("EO-SOURCES/foo/main.eo");
         MatcherAssert.assertThat(
-                !Files.exists(out),
-                Matchers.is(true)
+            !Files.exists(out),
+            Matchers.is(true)
         );
     }
 
@@ -69,15 +93,15 @@ public class SkipTest {
         this.executeCopyMojo(temp, classes, false);
         final Path out = classes.resolve("EO-SOURCES/foo/main.eo");
         MatcherAssert.assertThat(
-                Files.exists(out),
-                Matchers.is(true)
+            Files.exists(out),
+            Matchers.is(true)
         );
     }
 
     private void executePullMojo(
-            @TempDir final Path temp,
-            final Path target,
-            final boolean skip
+        @TempDir final Path temp,
+        final Path target,
+        final boolean skip
     ) {
         final Path foreign = temp.resolve("eo-foreign.json");
         new MonoTojos(new Json(foreign))
@@ -97,9 +121,9 @@ public class SkipTest {
     }
 
     private void executeCopyMojo(
-            @TempDir final Path temp,
-            final Path classes,
-            final boolean skip
+        @TempDir final Path temp,
+        final Path classes,
+        final boolean skip
     ) throws IOException {
         final Path src = temp.resolve("src");
         new Save(
