37a38c00065181732ce1d823b016858548dac01c
MikhailLipanin <mikhail.lipanin@gmail.com>
2022-09-14 18:22:21 +0300

    Unix-like EOLs

--- eo-test/src/test/java/org/eolang/test/CheckXSLTest.java
+++ eo-test/src/test/java/org/eolang/test/CheckXSLTest.java
@@ -1,1 +1,14 @@
-package org.eolang.test;import org.hamcrest.MatcherAssert;import org.hamcrest.Matchers;import org.junit.jupiter.api.Test;public final class CheckXSLTest {    @Test    void check() {        MatcherAssert.assertThat(true, Matchers.is(true));    }}
+package org.eolang.test;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+public final class CheckXSLTest {
+
+    @Test
+    void check() {
+        MatcherAssert.assertThat(true, Matchers.is(true));
+    }
+
+}
