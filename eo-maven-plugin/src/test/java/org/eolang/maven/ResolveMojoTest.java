da19f47e4f7faf3ed08df8335b12a5f63de49fd4
volodya-lombrozo <volodya.lombrozo@yandex.ru>
2022-10-26 14:24:42 +0300

    feat(#934): fix failed test

--- eo-maven-plugin/src/test/java/org/eolang/maven/ResolveMojoTest.java
+++ eo-maven-plugin/src/test/java/org/eolang/maven/ResolveMojoTest.java
@@ -189,7 +189,7 @@ final class ResolveMojoTest {
             .with("skipZeroVersions", true)
             .with("discoverSelf", false)
             .with("ignoreVersionConflicts", true)
-            .with("plugin", new DcsFile.Dummy())
+            .with("dependencies", new DcsFile.Dummy())
             .execute();
         MatcherAssert.assertThat(
             true,
