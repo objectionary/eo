49ff0c78c768e25b00acda2bb7f1cb05d4f9f8db
mximp <mxim.petrov@gmail.com>
2022-11-13 23:18:33 +0300

    #1415 corrected pakage-info

--- eo-runtime/src/main/java/EOorg/EOeolang/package-info.java
+++ eo-runtime/src/main/java/EOorg/EOeolang/package-info.java
@@ -32,7 +32,4 @@
  * @see <a href="https://www.eolang.org">project site www.eolang.org</a>
  * @see <a href="https://github.com/objectionary/eo">GitHub project</a>
  */
-@PackageMarker
 package EOorg.EOeolang;
-
-import org.eolang.maven.PackageMarker;
