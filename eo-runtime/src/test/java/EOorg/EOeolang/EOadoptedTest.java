1b6d1ef9162644a15b84c5e28155799e91b6eecf
Yegor Bugayenko <yegor256@gmail.com>
2022-11-23 20:38:26 +0300

    #adopted: adopted object

---
+++ eo-runtime/src/test/java/EOorg/EOeolang/EOadoptedTest.java
@@ -0,0 +1,61 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2022 Objectionary.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * @checkstyle PackageNameCheck (10 lines)
+ */
+package EOorg.EOeolang;
+
+import org.eolang.Data;
+import org.eolang.Dataized;
+import org.eolang.PhWith;
+import org.eolang.Phi;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test case for {@link EOadopted}.
+ *
+ * @since 0.29
+ */
+public final class EOadoptedTest {
+
+    @Test
+    public void changesTheParent() {
+        MatcherAssert.assertThat(
+            new Dataized(
+                new PhWith(
+                    new PhWith(
+                        new EOadopted(Phi.Φ),
+                        "x", new EObool$EOnot(Phi.Φ)
+                    ),
+                    "p", new Data.ToPhi(true)
+                )
+            ).take(Boolean.class),
+            Matchers.is(false)
+        );
+    }
+
+}
