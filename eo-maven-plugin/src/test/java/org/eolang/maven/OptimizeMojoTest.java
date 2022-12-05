b8d0f6e3778b2e9bbf1b9ee9ffb50aa0204861ff
volodya-lombrozo <volodya.lombrozo@yandex.ru>
2022-12-05 12:54:29 +0300

    feat(#1517): add withHelloWorld program to FakeMaven

--- eo-maven-plugin/src/test/java/org/eolang/maven/OptimizeMojoTest.java
+++ eo-maven-plugin/src/test/java/org/eolang/maven/OptimizeMojoTest.java
@@ -56,11 +56,7 @@ final class OptimizeMojoTest {
     @Test
     void skipsAlreadyOptimized(@TempDir final Path temp) throws Exception {
         final FakeMaven maven = new FakeMaven(temp)
-            .withProgram(
-                "+package f",
-                "[args] > main",
-                "  (stdout \"Hello!\").print > @"
-            )
+            .withHelloWorld()
             .execute(ParseMojo.class)
             .execute(OptimizeMojo.class);
         final Path path = maven.result().get(
@@ -78,11 +74,7 @@ final class OptimizeMojoTest {
     void optimizesIfExpired(@TempDir final Path temp) throws Exception {
         final FakeMaven maven = new FakeMaven(temp);
         final Path tgt = maven
-            .withProgram(
-                "+package f",
-                "[args] > main",
-                "  (stdout \"Hello!\").print > @"
-            )
+            .withHelloWorld()
             .execute(ParseMojo.class)
             .execute(OptimizeMojo.class)
             .result()
@@ -120,11 +112,7 @@ final class OptimizeMojoTest {
                 .resolve("foo/x/main.xmir")
         );
         new FakeMaven(temp)
-            .withProgram(
-                "+package f",
-                "[args] > main",
-                "  (stdout \"Hello!\").print > @"
-            )
+            .withHelloWorld()
             .with("cache", cache)
             .withTojoAttribute(AssembleMojo.ATTR_HASH, hash)
             .execute(ParseMojo.class)
@@ -150,11 +138,7 @@ final class OptimizeMojoTest {
         final Path cache = temp.resolve("cache");
         final String hash = "abcdef1";
         new FakeMaven(temp)
-            .withProgram(
-                "+package f",
-                "[args] > main",
-                "  (stdout \"Hello!\").print > @"
-            )
+            .withHelloWorld()
             .with("cache", cache)
             .withTojoAttribute(AssembleMojo.ATTR_HASH, hash)
             .execute(ParseMojo.class)
@@ -171,11 +155,7 @@ final class OptimizeMojoTest {
     void optimizesSuccessfully(@TempDir final Path temp) throws Exception {
         final FakeMaven maven = new FakeMaven(temp);
         final Map<String, Path> res = maven
-            .withProgram(
-                "+package f",
-                "[args] > main",
-                "  (stdout \"Hello!\").print > @"
-            )
+            .withHelloWorld()
             .with("trackOptimizationSteps", true)
             .execute(ParseMojo.class)
             .execute(OptimizeMojo.class)
@@ -205,11 +185,7 @@ final class OptimizeMojoTest {
         final FakeMaven maven = new FakeMaven(temp);
         final int total = 20;
         for (int program = 0; program < total; ++program) {
-            maven.withProgram(
-                "+package f",
-                "[args] > main",
-                "  (stdout \"Hello!\").print"
-            );
+            maven.withHelloWorld();
         }
         final Map<String, Path> res = maven
             .execute(ParseMojo.class)
