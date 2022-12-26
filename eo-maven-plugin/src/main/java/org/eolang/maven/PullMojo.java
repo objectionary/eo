09fb5337142492789b1393f37fc7e2e98289f472
MikhailLipanin <mikhail.lipanin@gmail.com>
2022-12-26 13:59:40 +0300

    reset logging for `PullMojo`

--- eo-maven-plugin/src/main/java/org/eolang/maven/PullMojo.java
+++ eo-maven-plugin/src/main/java/org/eolang/maven/PullMojo.java
@@ -154,13 +154,17 @@ public final class PullMojo extends SafeMojo {
                 new ChNarrow(hash).value()
             );
         }
-        if (tojos.isEmpty()) {
-            if (this.scopedTojos().select(row -> true).isEmpty()) {
-                Logger.warn(this, "Nothing to pull, since there are no foreign programs");
-            } else {
-                Logger.info(this, "Nothing to pull, all programs pulled already");
+        if (!tojos.isEmpty()) {
+            for (final Tojo tojo : tojos) {
+                tojo.set(
+                    AssembleMojo.ATTR_EO,
+                    this.pull(tojo.get(Tojos.KEY)).toAbsolutePath().toString()
+                );
+                tojo.set(
+                    AssembleMojo.ATTR_HASH,
+                    new ChNarrow(hash).value()
+                );
             }
-        } else {
             Logger.info(
                 this, "%d program(s) pulled from %s",
                 tojos.size(), this.objectionary
