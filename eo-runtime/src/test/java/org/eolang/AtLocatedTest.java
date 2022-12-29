de84f365a2835e062745b3e740afbc56de5ef3b0
volodya-lombrozo <volodya.lombrozo@gmail.com>
2022-12-29 18:23:05 +0300

    feat(#1618): fix all qulice suggestions

--- eo-runtime/src/test/java/org/eolang/AtLocatedTest.java
+++ eo-runtime/src/test/java/org/eolang/AtLocatedTest.java
@@ -1,12 +1,38 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2022 Objectionary.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package org.eolang;
 
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
-import org.hamcrest.core.IsEqual;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Test case for {@link AtLocated}.
+ *
+ * @since 0.29.0
+ */
 class AtLocatedTest {
 
     @Test
@@ -24,7 +50,6 @@ class AtLocatedTest {
             new AtLocated(origin, 10, 10).φTerm(),
             Matchers.equalTo(origin.φTerm())
         );
-
     }
 
     @Test
@@ -34,7 +59,6 @@ class AtLocatedTest {
             origin.copy(Phi.Φ).toString(),
             Matchers.equalTo(origin.toString())
         );
-
     }
 
     @Test
@@ -61,37 +85,51 @@ class AtLocatedTest {
         );
     }
 
-    private static class AtFailed implements Attr {
+    /**
+     * Attribute that trows an exception on all methods.
+     *
+     * @since 0.29.0
+     */
+    private static final class AtFailed implements Attr {
 
+        /**
+         * Exception to throw.
+         */
         private final ExAbstract exception;
 
-        AtFailed() {
+        /**
+         * Ctor.
+         */
+        private AtFailed() {
             this(new ExFlow());
         }
 
-        public AtFailed(final ExAbstract ex) {
-            this.exception = ex;
+        /**
+         * Ctor.
+         * @param except Exception to throw.
+         */
+        private AtFailed(final ExAbstract except) {
+            this.exception = except;
         }
 
         @Override
         public Attr copy(final Phi self) {
-            throw exception;
+            throw this.exception;
         }
 
         @Override
         public Phi get() {
-            throw exception;
+            throw this.exception;
         }
 
         @Override
         public void put(final Phi phi) {
-            throw exception;
-
+            throw this.exception;
         }
 
         @Override
         public String φTerm() {
-            throw exception;
+            throw this.exception;
         }
     }
 }
