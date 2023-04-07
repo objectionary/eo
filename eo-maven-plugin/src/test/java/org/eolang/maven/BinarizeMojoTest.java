2af1fcd8114a38334ed5523936331dd731ff107e
levBagryansky <28lev11@gmail.com>
2023-04-07 14:23:57 +0300

    #1965 removed r

--- eo-maven-plugin/src/test/java/org/eolang/maven/BinarizeMojoTest.java
+++ eo-maven-plugin/src/test/java/org/eolang/maven/BinarizeMojoTest.java
@@ -40,7 +40,7 @@ import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.junit.jupiter.params.ParameterizedTest;
 
 /**
- * Test case for {@link BinarizeMojo}.
+ * Test case fo {@link BinarizeMojo}.
  *
  * @since 0.1
  */
