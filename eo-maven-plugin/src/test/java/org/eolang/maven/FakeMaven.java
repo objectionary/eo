bc9c57ee72bf882ed1020108f8572148c167817c
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-05-19 17:36:58 +0300

    feat(#2072): Remove unused imports

--- eo-maven-plugin/src/test/java/org/eolang/maven/FakeMaven.java
+++ eo-maven-plugin/src/test/java/org/eolang/maven/FakeMaven.java
@@ -37,7 +37,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
