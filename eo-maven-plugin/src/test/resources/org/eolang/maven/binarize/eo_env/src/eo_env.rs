c8b1550b1d268105f0e9536bfb1afb913776cc9a
levBagryansky <28lev11@gmail.com>
2023-07-14 16:47:58 +0300

    #2237: Dummy eo_env dependency for tests

---
+++ eo-maven-plugin/src/test/resources/org/eolang/maven/binarize/eo_env/src/eo_env.rs
@@ -0,0 +1,19 @@
+use jni::JNIEnv;
+use jni::objects::{JClass, JObject, JValue};
+
+#[allow(dead_code)]
+pub struct EOEnv<'local> {
+    java_env: JNIEnv<'local>,
+    java_class: JClass<'local>,
+    java_obj: JObject<'local>
+}
+
+impl<'local> EOEnv<'_> {
+    pub fn new(java_env: JNIEnv<'local>, java_class: JClass<'local>, java_obj: JObject<'local>) -> EOEnv<'local> {
+        EOEnv {
+            java_env,
+            java_class,
+            java_obj
+        }
+    }
+}
