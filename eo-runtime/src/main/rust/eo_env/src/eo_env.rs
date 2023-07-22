aa516d4f17ba8b51e3beed724ae9fa49320418b4
levBagryansky <28lev11@gmail.com>
2023-07-23 02:32:18 +0300

    #2283: marked unused field

--- eo-runtime/src/main/rust/eo_env/src/eo_env.rs
+++ eo-runtime/src/main/rust/eo_env/src/eo_env.rs
@@ -26,18 +26,17 @@ pub mod eo_enum;
 use jni::JNIEnv;
 use jni::objects::{JClass, JObject, JValue};
 
-#[allow(dead_code)]
 pub struct EOEnv<'local> {
     pub java_env: JNIEnv<'local>,
-    java_class: JClass<'local>,
+    _java_class: JClass<'local>,
     java_obj: JObject<'local>
 }
 
 impl<'local> EOEnv<'_> {
-    pub fn new(java_env: JNIEnv<'local>, java_class: JClass<'local>, java_obj: JObject<'local>) -> EOEnv<'local> {
+    pub fn new(java_env: JNIEnv<'local>, _java_class: JClass<'local>, java_obj: JObject<'local>) -> EOEnv<'local> {
         EOEnv {
             java_env,
-            java_class,
+            _java_class,
             java_obj
         }
     }
