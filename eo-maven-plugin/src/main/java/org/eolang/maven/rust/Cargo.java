e4375083a612f50e4aedda1238c92bbe319e6df3
levBagryansky <28lev11@gmail.com>
2023-07-24 17:42:01 +0300

    #2239: Violation solved.

--- eo-maven-plugin/src/main/java/org/eolang/maven/rust/Cargo.java
+++ eo-maven-plugin/src/main/java/org/eolang/maven/rust/Cargo.java
@@ -35,10 +35,10 @@ import org.cactoos.map.MapOf;
 
 /**
  * Class to manipulate Cargo.toml file.
+ * @since 1.0
  * @todo #2239:60 min. Make this class extending Savable class.
  *  This class have save method and needs to have explanatory
  *  comment too via Commented class.
- * @since 1.0
  */
 public class Cargo {
     /**
