0d833f8a1db586c173b6c516ac14d91a2613264b
maxonfjvipon <mtrunnikov@gmail.com>
2023-07-25 11:18:45 +0300

    #2302 - default objectionaries

---
+++ eo-maven-plugin/src/main/java/org/eolang/maven/objectionary/OjsDefault.java
@@ -0,0 +1,64 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2023 Objectionary.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.eolang.maven.objectionary;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Default objectionaries.
+ * @since 0.29.6
+ */
+public class OjsDefault implements Objectionaries {
+    /**
+     * Hash-map.
+     */
+    private final Map<String, Objectionary> map;
+
+    /**
+     * Ctor.
+     */
+    public OjsDefault() {
+        this(new HashMap<>());
+    }
+
+    /**
+     * Ctor.
+     * @param ojs Objectionaries hash-map.
+     */
+    OjsDefault(final Map<String, Objectionary> ojs) {
+        this.map = ojs;
+    }
+
+    @Override
+    public Objectionaries with(String hash, Objectionary objectionary) {
+        this.map.putIfAbsent(hash, objectionary);
+        return this;
+    }
+
+    @Override
+    public Objectionary get(String hash) {
+        return this.map.get(hash);
+    }
+}
