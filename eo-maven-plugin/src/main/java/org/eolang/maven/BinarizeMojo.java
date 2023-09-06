2413e774161032ca7d62029f59ee6a4f723bcc56
maxonfjvipon <mtrunnikov@gmail.com>
2023-09-06 10:49:18 +0300

    feat(#2399): comment

--- eo-maven-plugin/src/main/java/org/eolang/maven/BinarizeMojo.java
+++ eo-maven-plugin/src/main/java/org/eolang/maven/BinarizeMojo.java
@@ -112,7 +112,7 @@ public final class BinarizeMojo extends SafeMojo {
     /**
      * Is the project valid?
      * @param project File to check.
-     * @return True if valid. Otherwise false.
+     * @return True if valid. Otherwise, false.
      */
     private static boolean valid(final File project) {
         return project.isDirectory()
