c30eeaf031315202e80eb2f8ec58e388030328a3
levBagryansky <28lev11@gmail.com>
2023-11-21 16:12:23 +0300

    #2558: %s

--- eo-runtime/src/main/java/org/eolang/Dataized.java
+++ eo-runtime/src/main/java/org/eolang/Dataized.java
@@ -111,7 +111,7 @@ public final class Dataized {
             final Object data = Data.class.cast(src).take();
             if (!(data instanceof byte[])) {
                 throw new ExFailure(
-                    "data of must be %s, but was %s",
+                    "data of %s must be %s, but was %s",
                     this.phi.toString(),
                     byte[].class,
                     data.getClass()
