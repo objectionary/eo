e29284f7789fc5bef792362bc5fb13c9e0fbfbea
maxonfjvipon <mtrunnikov@gmail.com>
2023-11-22 18:39:43 +0300

    feat(#2612): seq test

--- eo-runtime/src/test/java/EOorg/EOeolang/EOseqTest.java
+++ eo-runtime/src/test/java/EOorg/EOeolang/EOseqTest.java
@@ -81,31 +81,4 @@ public final class EOseqTest {
             Matchers.startsWith("Hello")
         );
     }
-
-    @Test
-    public void makesTrueCopy() {
-        final Phi first = new EOseq(Phi.Φ);
-        first.attr(0).put(
-            new PhWith(
-                new EOtuple$EOempty(Phi.Φ).attr("with").get().copy(),
-                0, new Data.ToPhi(1L)
-            )
-        );
-        final Phi second = first.copy();
-        second.attr(0).put(
-            new PhWith(
-                new EOtuple$EOempty(Phi.Φ).attr("with").get().copy(),
-                0, new Data.ToPhi(2L)
-            )
-        );
-        MatcherAssert.assertThat(
-            new Dataized(first).take(Long.class),
-            Matchers.equalTo(1L)
-        );
-        MatcherAssert.assertThat(
-            new Dataized(second).take(Long.class),
-            Matchers.equalTo(2L)
-        );
-    }
-
 }
