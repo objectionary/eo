8d43a83178d64fd3eabac98b6731140d900669b4
Roman Korostinskiy <70313618+c71n93@users.noreply.github.com>
2023-11-28 12:31:33 +0300

    #2591 fix todo

--- eo-runtime/src/main/java/org/eolang/Main.java
+++ eo-runtime/src/main/java/org/eolang/Main.java
@@ -172,8 +172,8 @@ public final class Main {
      * @param opts The opts left
      * @throws Exception If fails
      * @todo #2591:60min Make type casting when logging an app dataization result. The cast is
-     * necessary in this expression {@code new Data set(app).take()} in order to correctly convert
-     * it to a string. This conversion it necessary for proper logging.
+     *  necessary in this expression {@code new Data set(app).take()} in order to correctly
+     *  convert it to a string. This conversion it necessary for proper logging.
      */
     private static void run(final List<String> opts) throws Exception {
         final String path = new JavaPath(opts.get(0)).toString();
