e1d2fbe01d9eefe863a113d7bce1eff8837d95a2
levBagryansky <28lev11@gmail.com>
2023-12-05 16:31:23 +0300

    #2443: failOnError flag

--- eo-maven-plugin/src/main/java/org/eolang/maven/AssembleMojo.java
+++ eo-maven-plugin/src/main/java/org/eolang/maven/AssembleMojo.java
@@ -116,7 +116,7 @@ public final class AssembleMojo extends SafeMojo {
 
     /**
      * Whether we should fail on error.
-     * @checkstyle MemberNameCheck (7 lines)
+     * @checkstyle MemberNameCheck (11 lines)
      * @since 0.23.0
      * @todo #2443:90min Remove the following failOnError flag. Now we
      *  have already got rid from it in {@link OptimizeMojo} and {@link VerifyMojo}.
