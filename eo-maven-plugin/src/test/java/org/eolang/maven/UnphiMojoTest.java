8be9dd69a8cf13beede9fa8300cda9b0174b48ba
maxonfjvipon <mtrunnikov@gmail.com>
2023-12-28 13:59:20 +0300

    fix(#2729): import

--- eo-maven-plugin/src/test/java/org/eolang/maven/UnphiMojoTest.java
+++ eo-maven-plugin/src/test/java/org/eolang/maven/UnphiMojoTest.java
@@ -41,7 +41,6 @@ import org.eolang.parser.EoSyntax;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import org.junit.jupiter.params.ParameterizedTest;
