6dba02216de0fece87e2be274d0307646ed1c5da
Roman Korostinskiy <70313618+c71n93@users.noreply.github.com>
2024-01-16 18:38:40 +0300

    #2723 remove todo

--- eo-runtime/src/test/java/org/eolang/MainTest.java
+++ eo-runtime/src/test/java/org/eolang/MainTest.java
@@ -40,12 +40,6 @@ import org.junit.jupiter.api.Test;
  * Test case for {@link Main}.
  *
  * @since 0.1
- * @todo #2718:30min Enable the tests. Tests {@link MainTest#deliversCleanOutput()} and
- *  {@link MainTest#executesJvmFullRun()} were disabled because they execute "org.eolang.io.stdout"
- *  object that accepts "string". But arguments after "org.eolang.io.stdout" are stored into "tuple"
- *  and are being passed as "tuple" object. And here we get the situation where
- *  "stdout" accepts "tuple" and fails. We need to enable the test by finding object that accepts
- *  "tuple" as the first argument, or make a custom one.
  */
 final class MainTest {
 
