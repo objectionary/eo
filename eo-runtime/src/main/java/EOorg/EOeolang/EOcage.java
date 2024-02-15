d8afc2be14fbdf9f78b863a53ef503e30ca8abc1
levBagryansky <28lev11@gmail.com>
2024-02-16 01:51:07 +0300

    #2836: Edited todo

--- eo-runtime/src/main/java/EOorg/EOeolang/EOcage.java
+++ eo-runtime/src/main/java/EOorg/EOeolang/EOcage.java
@@ -116,7 +116,7 @@ public final class EOcage extends PhDefault implements Atom {
     /**
      * Class to trace if the cage got into recursion during the dataization.
      * @since 0.36
-     * @todo #2836: 60min Add a new parameter of recursion depth. This parameter
+     * @todo #2836:60min Add a new parameter of recursion depth. This parameter
      *   should be set by user via pom.xml. We can make DATAIZING_CAGES a
      *   ConsurrentHashMap which will add thread safety to the class.
      */
