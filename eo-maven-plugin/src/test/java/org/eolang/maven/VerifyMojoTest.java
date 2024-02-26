dd2952e8b3ef69d80b74457d712d680e9d8dfd22
levBagryansky <28lev11@gmail.com>
2024-02-26 16:28:23 +0300

    #2890: violation

--- eo-maven-plugin/src/test/java/org/eolang/maven/VerifyMojoTest.java
+++ eo-maven-plugin/src/test/java/org/eolang/maven/VerifyMojoTest.java
@@ -50,7 +50,8 @@ import org.junit.jupiter.api.io.TempDir;
  *  /org/eolang/parser/warnings/mandatory-version-meta.xsl and
  *  /org/eolang/parser/warnings/mandatory-home-meta.xsl.
  *  After you need fix {@code createRegEx()}.
- * @todo #2890:30min Fix this {@link VerifyMojoTest#detectsErrorsSuccessfully} flaky test and enable it. It failed in ci
+ * @todo #2890:30min Fix this {@link VerifyMojoTest#detectsErrorsSuccessfully}
+ *  flaky test and enable it. It failed in ci
  *  <a href="https://github.com/objectionary/eo/actions/runs/8041230784/job/21960239171?pr=2892">here</a>
  *  without providing the regex and message. Also may be it would be cleaner to fix
  *  error Assertion since now it is hard to get why it failed.
