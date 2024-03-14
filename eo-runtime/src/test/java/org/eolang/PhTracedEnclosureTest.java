8a8337fdb5b5dd634aba3edc55ae6e787564aa9f
levBagryansky <28lev11@gmail.com>
2024-03-14 16:39:58 +0300

    #2891: Very simple test of PhTracedEnclosure api

--- eo-runtime/src/test/java/org/eolang/PhTracedEnclosureTest.java
+++ eo-runtime/src/test/java/org/eolang/PhTracedEnclosureTest.java
@@ -1,6 +1,4 @@
-import java.nio.file.Path
-
-/**
+/*
  * The MIT License (MIT)
  *
  * Copyright (c) 2016-2024 Objectionary.com
@@ -23,20 +21,43 @@ import java.nio.file.Path
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+package org.eolang;
+
+import EOorg.EOeolang.EOcage;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 /**
- * Entry point for running validation scripts.
- * To add new validation create new script in this folder and add it
- * to the list below.
+ * Test case for {@link PhTracedEnclosure}.
+ *
+ * @since 0.24
  */
-Path folder = basedir.toPath().resolve("src").resolve("test").resolve("groovy")
-tests = [
-  'check-folders-numbering.groovy',
-  'check-all-java-classes-compiled.groovy',
-  'check-runtime-deps.groovy'
-]
-for (it in tests) {
-  def res = evaluate folder.resolve(it).toFile()
-  println String.format('Verified with %s - OK. Result: %s', it, res)
+public class PhTracedEnclosureTest {
+
+    @Test
+    void throwsIfZeroDepth() {
+        final PhTracedEnclosure traced = new PhTracedEnclosure(
+            new Data.ToPhi(0L), new EOcage(Phi.Φ), 0
+        );
+        Assertions.assertThrows(
+            ExAbstract.class,
+            () -> new Dataized(traced).take(),
+            "We expect that dataization is not allowed if depth is less than 0"
+        );
+    }
+
+    @Test
+    void allowsSinglePass() {
+        final long data = 0L;
+        final PhTracedEnclosure traced = new PhTracedEnclosure(
+            new Data.ToPhi(data), new EOcage(Phi.Φ), 1
+        );
+        MatcherAssert.assertThat(
+            "dataization of object without recursion is allowed if depth is more than 0",
+            new Dataized(traced).take(Long.class),
+            Matchers.equalTo(data)
+        );
+    }
 }
-true
