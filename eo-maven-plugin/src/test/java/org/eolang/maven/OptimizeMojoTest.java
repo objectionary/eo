155452a0eef97d1499fb3b75bd706193a9217008
Roman Korostinskiy <70313618+c71n93@users.noreply.github.com>
2024-03-27 19:39:03 +0300

    #2999 enable test

--- eo-maven-plugin/src/test/java/org/eolang/maven/OptimizeMojoTest.java
+++ eo-maven-plugin/src/test/java/org/eolang/maven/OptimizeMojoTest.java
@@ -53,15 +53,11 @@ import org.junit.jupiter.params.ParameterizedTest;
 /**
  * Test case for {@link OptimizeMojo}.
  * @since 0.1
- * @todo #2931:30min Enable the test {@link OptimizeMojoTest#checksPacks(String)}. The test was
- *  disabled because java generation (to-java.xsl) was changed. Need to change .yaml packs and
- *  enable the test.
  */
 @SuppressWarnings({"PMD.AvoidDuplicateLiterals", "PMD.TooManyMethods"})
 final class OptimizeMojoTest {
 
     @ParameterizedTest
-    @Disabled
     @ClasspathSource(value = "org/eolang/maven/packs/", glob = "**.yaml")
     void checksPacks(final String pack) throws IOException {
         final CheckPack check = new CheckPack(pack);
