eeb63f53b57cc3640899fd10d9718c2b264df7b8
maxonfjvipon <mtrunnikov@gmail.com>
2024-08-01 23:14:35 +0300

    feat(#3251): file test

---
+++ eo-runtime/src/test/java/EOorg/EOeolang/EOfs/EOfileTest.java
@@ -0,0 +1,61 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2024 Objectionary.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * @checkstyle PackageNameCheck (10 lines)
+ */
+package EOorg.EOeolang.EOfs;
+
+import java.nio.file.Path;
+import org.eolang.Data;
+import org.eolang.Dataized;
+import org.eolang.PhWith;
+import org.eolang.Phi;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+/**
+ * Test case for {@link EOfile}.
+ * @since 0.40
+ */
+@SuppressWarnings("JTCOP.RuleAllTestsHaveProductionClass")
+final class EOfileTest {
+    @Test
+    void resolvesPath(@TempDir final Path temp) {
+        final Phi resolved = new PhWith(
+            new EOfile(),
+            "path", new Data.ToPhi(temp.toAbsolutePath().toString())
+        ).take("resolved").copy();
+        resolved.put(0, new Data.ToPhi("foo/bar/baz"));
+        MatcherAssert.assertThat(
+            "The `file.resolved` object should resolve in a right way",
+            new Dataized(resolved).asString(),
+            Matchers.equalTo(
+                temp.resolve("foo/bar/baz").toAbsolutePath().toString()
+            )
+        );
+    }
+}
