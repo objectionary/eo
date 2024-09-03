caf54c107aacbd131761311e1235b058c57d874e
maxonfjvipon <mtrunnikov@gmail.com>
2024-09-03 16:58:24 +0300

    fix(#3357): unused tag

--- eo-runtime/src/test/java/org/eolang/SnippetIT.java
+++ eo-runtime/src/test/java/org/eolang/SnippetIT.java
@@ -42,7 +42,6 @@ import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Assumptions;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.io.TempDir;
