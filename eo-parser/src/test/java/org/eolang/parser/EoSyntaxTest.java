283d6f27f11333195d5f00435e5c1f2e76fa43d4
h1alexbel <h1alexbelx@gmail.com>
2024-12-02 18:27:37 +0300

    feat(#3536): unused

--- eo-parser/src/test/java/org/eolang/parser/EoSyntaxTest.java
+++ eo-parser/src/test/java/org/eolang/parser/EoSyntaxTest.java
@@ -37,7 +37,6 @@ import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Assumptions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
