fe900ab65040b6700c3cd66d450905b8503fdd50
volodya-lombrozo <volodya.lombrozo@gmail.com>
2025-01-15 16:09:14 +0300

    feat(#3744): add more descriptive message for 'slave' object

--- eo-parser/src/main/java/org/eolang/parser/EoParserErrors.java
+++ eo-parser/src/main/java/org/eolang/parser/EoParserErrors.java
@@ -99,6 +99,8 @@ final class EoParserErrors extends BaseErrorListener implements Iterable<Parsing
                 detailed = "Invalid meta declaration";
             } else if (names[EoParser.RULE_program].equals(rule)) {
                 detailed = "Invalid program declaration";
+            } else if (names[EoParser.RULE_slave].equals(rule)) {
+                detailed = "Invalid objects declaration that may be used inside abstract object";
             } else {
                 detailed = "no viable alternative at input";
             }
