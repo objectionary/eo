16dcd8f6234f8dc216e868d7023297120e91c1c8
Andrew Smirnov <smirnov.andrew.1999@yandex.ru>
2025-01-17 09:46:10 +0300

    Use Expect in EOnumber$EOgt

---
+++ eo-runtime/src/test/java/EOorg/EOeolang/EOnumber$EOgtTest.java
@@ -0,0 +1,96 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2025 Objectionary.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * @checkstyle PackageNameCheck (10 lines)
+ * @checkstyle TrailingCommentCheck (3 lines)
+ */
+package EOorg.EOeolang; // NOPMD
+
+import org.eolang.Attr;
+import org.eolang.Data;
+import org.eolang.Dataized;
+import org.eolang.ExAbstract;
+import org.eolang.PhWith;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test case for {@link EOnumber$EOgt}.
+ *
+ * @since 0.51
+ * @checkstyle TypeNameCheck (3 lines)
+ */
+@SuppressWarnings("PMD.AvoidDollarSigns")
+final class EOnumber$EOgtTest {
+
+    @Test
+    void throwsCorrectErrorForXAttr() {
+        MatcherAssert.assertThat(
+            "the message in the error is correct",
+            Assertions.assertThrows(
+                ExAbstract.class,
+                () -> new Dataized(
+                    new PhWith(
+                        new PhWith(
+                            new EOnumber$EOgt(),
+                            Attr.RHO,
+                            new Data.ToPhi(42)
+                        ),
+                        "x",
+                        new Data.ToPhi(true)
+                    )
+                ).take(),
+                "comparison with TRUE fails with a proper message that explains what happened"
+            ).getMessage(),
+            Matchers.equalTo("the 'x' attribute must be a number")
+        );
+    }
+
+    @Test
+    void throwsCorrectErrorForRhoAttr() {
+        MatcherAssert.assertThat(
+            "the message in the error is correct",
+            Assertions.assertThrows(
+                ExAbstract.class,
+                () -> new Dataized(
+                    new PhWith(
+                        new PhWith(
+                            new EOnumber$EOdiv(),
+                            Attr.RHO,
+                            new Data.ToPhi(true)
+                        ),
+                        "x",
+                        new Data.ToPhi(42)
+                    )
+                ).take(),
+                "EOnumber must be a number"
+            ).getMessage(),
+            Matchers.equalTo("the 'ρ' attribute must be a number")
+        );
+    }
+
+}
