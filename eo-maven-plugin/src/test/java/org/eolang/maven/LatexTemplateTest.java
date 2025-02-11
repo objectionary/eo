0dd2ede690a47a06c357936c9c0933d2bebe2867
Suban05 <anatolyb94@gmail.com>
2025-02-11 19:55:58 +0500

    #2089 fix garbage

--- eo-maven-plugin/src/test/java/org/eolang/maven/LatexTemplateTest.java
+++ eo-maven-plugin/src/test/java/org/eolang/maven/LatexTemplateTest.java
@@ -124,16 +124,4 @@ final class LatexTemplateTest {
     private static String input(final String... inputs) {
         return String.join("\n", inputs);
     }
-                "\\documentclass{article}",
-                "\\usepackage{ffcode}",
-                "\\begin{document}",
-                "\\begin{ffcode}",
-                "+package f",
-                "[args] > main",
-                "  stdout \"Hello!\"",
-                "\\end{ffcode}",
-                "\\end{document}"
-            )
-        );
-    }
 }
