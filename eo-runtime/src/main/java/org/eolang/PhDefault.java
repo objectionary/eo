7556abe7a4fdf740b1b75716bfbb3c043e316d18
Suban05 <anatolyb94@gmail.com>
2025-02-21 23:32:52 +0500

    #3542 rename

--- eo-runtime/src/main/java/org/eolang/PhDefault.java
+++ eo-runtime/src/main/java/org/eolang/PhDefault.java
@@ -141,7 +141,7 @@ public class PhDefault implements Phi, Cloneable {
     @Override
     public void put(final int pos, final Phi object) {
         final String name = this.attr(pos);
-        if (!(((AtWithRho) this.attrs.get(name)).getOrigin() instanceof AtVoid)) {
+        if (!(((AtWithRho) this.attrs.get(name)).origin() instanceof AtVoid)) {
             throw new ExReadOnly(
                 String.format(
                     "Can't put attribute with position %d because it's not void one",
