98cae22d789a7c0f7fce3057a692935d12a7b8af
Yegor Bugayenko <yegor256@gmail.com>
2025-02-23 07:59:03 +0300

    MIT removed

--- eo-maven-plugin/src/main/java/org/eolang/maven/Moja.java
+++ eo-maven-plugin/src/main/java/org/eolang/maven/Moja.java
@@ -1,6 +1,4 @@
 /*
- * The MIT License (MIT)
- *
  * SPDX-FileCopyrightText: Copyright (c) 2016-2025 Objectionary.com
  * SPDX-License-Identifier: MIT
  */
--- eo-maven-plugin/src/test/groovy/verify.groovy
+++ eo-maven-plugin/src/test/groovy/verify.groovy
@@ -1,8 +1,6 @@
 import java.nio.file.Path
 
 /**
- * The MIT License (MIT)
- *
  * SPDX-FileCopyrightText: Copyright (c) 2016-2025 Objectionary.com
  * SPDX-License-Identifier: MIT
  */
