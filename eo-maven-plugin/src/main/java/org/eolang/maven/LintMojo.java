4816f74e59465aa33ccf732170a72a6840a3848a
h1alexbel <h1alexbelx@gmail.com>
2025-03-03 11:39:36 +0300

    feat(#3977): puzzle

--- eo-maven-plugin/src/main/java/org/eolang/maven/LintMojo.java
+++ eo-maven-plugin/src/main/java/org/eolang/maven/LintMojo.java
@@ -259,10 +259,10 @@ public final class LintMojo extends SafeMojo {
      * @param xmir The XML before linting
      * @param counts Counts of errors, warnings, and critical
      * @return XML after linting
-     * @todo #3934:35min Remove .without() from Program to enable `unknown-metas`
-     *  and `unsorted-metas` lints. Currently we disabled them since lints does not
-     *  support `+spdx` meta yet. Once <a href="https://github.com/objectionary/lints/issues/354">this</a>
-     *  issue will be resolved, we should enable all lints.
+     * @todo #3977:25min Enable `unlint-non-existing-defect` lint.
+     *  Currently its disabled because of <a href="https://github.com/objectionary/lints/issues/354">this</a>
+     *  bug. Once issue will be resolved, we should enable this lint. Don't forget to enable
+     *  this lint in WPA scope too.
      */
     private static XML linted(final XML xmir, final ConcurrentHashMap<Severity, Integer> counts) {
         final Directives dirs = new Directives();
