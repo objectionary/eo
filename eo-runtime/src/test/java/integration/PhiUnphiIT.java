89f86823a83e25637cf7d66a1d8a5592a2ab6905
maxonfjvipon <mtrunnikov@gmail.com>
2025-03-12 23:19:40 +0300

    bug(#3199): pdd + typo

--- eo-runtime/src/test/java/integration/PhiUnphiIT.java
+++ eo-runtime/src/test/java/integration/PhiUnphiIT.java
@@ -24,7 +24,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
  * Integration test for phi-unphi.
  *
  * @since 0.1
- * @todo #3199:Enable PhiUnphiIT. The test was disabled because tuples are converted to EO
+ * @todo #3199:30min Enable PhiUnphiIT. The test was disabled because tuples are converted to EO
  *  incorrectly after phi-unphi. The key problem is recursive representation of tuples via
  *  tuple.with method. It should be done via simple application of tuple like it was done before,
  *  but with calculated length.
