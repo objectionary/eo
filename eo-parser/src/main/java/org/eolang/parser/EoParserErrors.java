f8c68de2e5c0cbe460e9b2c2e4d8783c3a918b71
Vladislav Samsonov <vl4d.s4m@gmail.com>
2025-03-19 23:50:18 +0300

    Simplify syntaxError in EoParserErrors.java

--- eo-parser/src/main/java/org/eolang/parser/EoParserErrors.java
+++ eo-parser/src/main/java/org/eolang/parser/EoParserErrors.java
@@ -51,13 +51,7 @@ final class EoParserErrors extends BaseErrorListener implements Iterable<Parsing
     }
 
     // @checkstyle ParameterNumberCheck (20 lines)
-    // @checkstyle CyclomaticComplexityCheck (100 lines)
-    // @todo #3744:30min Simplify {@link EoParserErrors#syntaxError} method.
-    //  The method is too complex and has a high cognitive complexity. We need to simplify it.
-    //  Don't forget to remove the @checkstyle CyclomaticComplexityCheck annotation and
-    //  the @SuppressWarnings("PMD.CognitiveComplexity") annotation.
     @Override
-    @SuppressWarnings("PMD.CognitiveComplexity")
     public void syntaxError(
         final Recognizer<?, ?> recognizer,
         final Object symbol,
@@ -73,66 +67,76 @@ final class EoParserErrors extends BaseErrorListener implements Iterable<Parsing
                 )
             );
         }
-        final List<String> msgs = new ArrayList<>(0);
+        final String result;
+        final String underlined;
         if (error instanceof NoViableAltException) {
-            final Token token = (Token) symbol;
-            final Parser parser = (Parser) recognizer;
-            final String rule = parser.getRuleInvocationStack().get(0);
-            final String[] names = parser.getRuleNames();
-            final String detailed;
-            if (names[EoParser.RULE_objects].equals(rule)) {
-                detailed = "Invalid object list declaration";
-            } else if (names[EoParser.RULE_metas].equals(rule)) {
-                detailed = "Invalid meta declaration";
-            } else if (names[EoParser.RULE_program].equals(rule)) {
-                detailed = "Invalid program declaration";
-            } else if (names[EoParser.RULE_bound].equals(rule)) {
-                detailed = "Invalid bound object declaration";
-            } else if (names[EoParser.RULE_object].equals(rule)) {
-                detailed = "Invalid object declaration";
-            } else {
-                detailed = "no viable alternative at input";
-            }
-            msgs.add(new MsgLocated(line, position, detailed).formatted());
-            msgs.add(
-                new MsgUnderlined(
-                    this.lines.line(line),
-                    position,
-                    Math.max(token.getStopIndex() - token.getStartIndex(), 1)
-                ).formatted()
-            );
+            result = getDetailedNoViableAlt((Parser) recognizer);
+            underlined = this.getMsgUnderlined(symbol, line, position);
         } else if (error instanceof InputMismatchException) {
-            final Token token = (Token) symbol;
-            final Parser parser = (Parser) recognizer;
-            final String rule = parser.getRuleInvocationStack().get(0);
-            final String detailed;
-            final String[] names = parser.getRuleNames();
-            if (names[EoParser.RULE_program].equals(rule)) {
-                detailed =
-                    "We expected the program to end here but encountered something unexpected";
-            } else if (names[EoParser.RULE_objects].equals(rule)) {
-                detailed =
-                    "We expected a list of objects here but encountered something unexpected";
-            } else {
-                detailed = msg;
-            }
-            msgs.add(new MsgLocated(line, position, detailed).formatted());
-            msgs.add(
-                new MsgUnderlined(
-                    this.lines.line(line),
-                    position,
-                    Math.max(token.getStopIndex() - token.getStartIndex(), 1)
-                ).formatted()
-            );
+            result = getDetailedInputMismatch((Parser) recognizer, msg);
+            underlined = this.getMsgUnderlined(symbol, line, position);
         } else {
-            msgs.add(new MsgLocated(line, position, msg).formatted());
-            msgs.add(this.lines.line(line));
+            result = msg;
+            underlined = this.lines.line(line);
         }
+        final List<String> msgs = List.of(
+            new MsgLocated(line, position, result).formatted(),
+            underlined
+        );
         this.errors.add(new ParsingException(error, line, msgs));
     }
 
     @Override
     public Iterator<ParsingException> iterator() {
         return this.errors.iterator();
+    }
+
+    private static String getDetailedNoViableAlt(final Parser parser) {
+        final String rule = parser.getRuleInvocationStack().get(0);
+        final String[] names = parser.getRuleNames();
+        final String detailed;
+        if (names[EoParser.RULE_objects].equals(rule)) {
+            detailed = "Invalid object list declaration";
+        } else if (names[EoParser.RULE_metas].equals(rule)) {
+            detailed = "Invalid meta declaration";
+        } else if (names[EoParser.RULE_program].equals(rule)) {
+            detailed = "Invalid program declaration";
+        } else if (names[EoParser.RULE_bound].equals(rule)) {
+            detailed = "Invalid bound object declaration";
+        } else if (names[EoParser.RULE_object].equals(rule)) {
+            detailed = "Invalid object declaration";
+        } else {
+            detailed = "no viable alternative at input";
+        }
+        return detailed;
+    }
+
+    private static String getDetailedInputMismatch(final Parser parser, final String msg) {
+        final String rule = parser.getRuleInvocationStack().get(0);
+        final String[] names = parser.getRuleNames();
+        final String detailed;
+        if (names[EoParser.RULE_program].equals(rule)) {
+            detailed =
+                "We expected the program to end here but encountered something unexpected";
+        } else if (names[EoParser.RULE_objects].equals(rule)) {
+            detailed =
+                "We expected a list of objects here but encountered something unexpected";
+        } else {
+            detailed = msg;
+        }
+        return detailed;
+    }
+
+    private String getMsgUnderlined(
+        final Object symbol,
+        final int line,
+        final int position
+    ) {
+        final Token token = (Token) symbol;
+        return new MsgUnderlined(
+            this.lines.line(line),
+            position,
+            Math.max(token.getStopIndex() - token.getStartIndex(), 1)
+        ).formatted();
     }
 }
