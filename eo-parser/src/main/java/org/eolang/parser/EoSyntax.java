fa1f1a537378ce02b1d10e8384e46a318bbe272c
maxonfjvipon <mtrunnikov@gmail.com>
2025-04-08 16:05:19 +0300

    bug(#3731): xsl

--- eo-parser/src/main/java/org/eolang/parser/EoSyntax.java
+++ eo-parser/src/main/java/org/eolang/parser/EoSyntax.java
@@ -101,7 +101,8 @@ public final class EoSyntax implements Syntax {
                     "/org/eolang/parser/parse/resolve-aliases.xsl",
                     "/org/eolang/parser/parse/add-default-package.xsl",
                     "/org/eolang/parser/parse/roll-bases.xsl",
-                    "/org/eolang/parser/parse/cti-adds-errors.xsl"
+                    "/org/eolang/parser/parse/cti-adds-errors.xsl",
+                    "/org/eolang/parser/parse/decorate.xsl"
                 ).back(),
                 new TrDefault<>(new StHex())
             )
