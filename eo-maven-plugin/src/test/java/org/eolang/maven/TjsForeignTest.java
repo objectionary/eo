b25e6d18f5250d25c7505ac6205a64452aa363b9
h1alexbel <h1alexbelx@gmail.com>
2025-05-26 10:15:02 +0200

    bug(#4129): clean for qulice

--- eo-maven-plugin/src/test/java/org/eolang/maven/TjsForeignTest.java
+++ eo-maven-plugin/src/test/java/org/eolang/maven/TjsForeignTest.java
@@ -6,9 +6,7 @@ package org.eolang.maven;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.cactoos.Func;
