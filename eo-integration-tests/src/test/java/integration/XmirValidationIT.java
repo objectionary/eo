a05ac66f96ae30f4c6bc73453dbc575e674772f8
h1alexbel <h1alexbelx@gmail.com>
2025-08-22 11:28:05 +0200

    bug(#4466): test

---
+++ eo-integration-tests/src/test/java/integration/XmirValidationIT.java
@@ -0,0 +1,49 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2016-2025 Objectionary.com
+ * SPDX-License-Identifier: MIT
+ */
+package integration;
+
+import com.jcabi.xml.XMLDocument;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.eolang.parser.StrictXmir;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Integration test for checking validity of parsed EO as XMIR documents.
+ *
+ * @since 0.58.3
+ */
+final class XmirValidationIT {
+
+    @Test
+    void checksWithXsd() throws IOException {
+        Files.walk(
+                Paths.get("").toAbsolutePath().getParent()
+                    .resolve("eo-runtime")
+                    .resolve("target")
+                    .resolve("eo")
+                    .resolve("1-parse")
+            ).filter(Files::isRegularFile)
+            .forEach(
+                xmir -> {
+                    try {
+                        Assertions.assertDoesNotThrow(
+                            new StrictXmir(
+                                new XMLDocument(xmir)
+                            )::inner,
+                            "validation should pass as normal"
+                        );
+                    } catch (final FileNotFoundException exception) {
+                        throw new IllegalStateException(
+                            String.format("Failed to find XMIR for %s", xmir), exception
+                        );
+                    }
+                }
+            );
+    }
+}
