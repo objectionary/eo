1c23d33327aa21edfa13dfd190d7ff7cfdd2945f
uchitsa <uchitsa@ya.ru>
2025-10-28 17:15:35 +0300

    #4182 fix

--- eo-maven-plugin/src/test/java/org/eolang/maven/ChTextTest.java
+++ eo-maven-plugin/src/test/java/org/eolang/maven/ChTextTest.java
@@ -14,7 +14,6 @@ import org.cactoos.io.ResourceOf;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
