baf9882090ae70521b3d074271ff321825155cf3
MrEx3cut0r <legendary.killtell@gmail.com>
2026-01-12 17:45:05 +0300

    fix

--- eo-runtime/src/test/java/org/eolang/SprintfTest.java
+++
@@ -1,9 +0,0 @@
-package org.eolang;
-import org.junit.jupiter.api.Test;
-
-public class SprintfTest {
-    @Test
-    public void testBasic() {
-        System.out.println("If this compiles, sprintf.eo is valid");
-    }
-}
