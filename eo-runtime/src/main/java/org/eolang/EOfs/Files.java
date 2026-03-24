bb357f2868450d668048e656b0eea912801a1795
Eset Shabakhov <eset-2000@mail.ru>
2026-03-24 21:37:44 +0500

    fix(objectionary#4904): synchronized replaced with lock in Files

--- eo-runtime/src/main/java/org/eolang/EOfs/Files.java
+++ eo-runtime/src/main/java/org/eolang/EOfs/Files.java
@@ -18,17 +18,13 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
 import org.eolang.ExFailure;
 
 /**
  * File streams.
  * @since 0.40
- * @todo #4884:30min Use ReentrantLock instead of 'synchronized' in Files.
- *  We should use ReentrantLock instead of 'synchronized' to avoid potential
- *  deadlocks when multiple AtOnce attributes are used together.
- *  Moreover, 'synchronized' keyword is forbidden by qulice.
  */
-@SuppressWarnings("PMD.AvoidSynchronizedStatement")
 final class Files {
     /**
      * Files instance.
@@ -40,12 +36,18 @@ final class Files {
      */
     @SuppressWarnings("PMD.LooseCoupling")
     private final ConcurrentHashMap<String, Object[]> streams;
+    
+    /**
+     * Lock.
+     */
+    private final Lock lock;
 
     /**
      * Ctor.
      */
     private Files() {
         this.streams = new ConcurrentHashMap<>(0);
+        this.lock = new ReentrantLock();
     }
 
     /**
@@ -74,7 +76,8 @@ final class Files {
      */
     @SuppressWarnings({"PMD.AssignmentInOperand", "PMD.CloseResource"})
     byte[] read(final String name, final int size) throws IOException {
-        synchronized (this.streams) {
+        this.lock.lock();
+        try {
             if (!this.streams.containsKey(name)) {
                 throw new ExFailure(
                     "File input stream with name %s is absent, can't read",
@@ -90,6 +93,8 @@ final class Files {
                 ++processed;
             }
             return Arrays.copyOf(read, processed);
+        } finally {
+            this.lock.unlock();
         }
     }
 
@@ -100,7 +105,8 @@ final class Files {
      * @throws IOException If fails to write
      */
     void write(final String name, final byte[] buffer) throws IOException {
-        synchronized (this.streams) {
+        this.lock.lock();
+        try {
             if (!this.streams.containsKey(name)) {
                 throw new ExFailure(
                     "File output stream with name %s is absent, can't read",
@@ -108,6 +114,8 @@ final class Files {
                 );
             }
             ((OutputStream) this.streams.get(name)[1]).write(buffer);
+        } finally {
+            this.lock.unlock();
         }
     }
 
@@ -117,7 +125,8 @@ final class Files {
      * @throws IOException If fails to close the streams
      */
     void close(final String name) throws IOException {
-        synchronized (this.streams) {
+        this.lock.lock();
+        try {
             if (!this.streams.containsKey(name)) {
                 throw new ExFailure(
                     "File streams with name %s is absent, can't close",
@@ -127,6 +136,8 @@ final class Files {
             ((InputStream) this.streams.get(name)[0]).close();
             ((OutputStream) this.streams.get(name)[1]).close();
             this.streams.remove(name);
+        } finally {
+            this.lock.unlock();
         }
     }
 }
