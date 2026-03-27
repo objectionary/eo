9c14d8db0ce0a46b087c0f4a63bc204cd2c60ffd
Eset Shabakhov <eset-2000@mail.ru>
2026-03-27 11:50:53 +0500

    #4953: moved asin from real to a separate object with tests

--- eo-runtime/src/main/java/org/eolang/EOms/EOasin.java
+++ eo-runtime/src/main/java/org/eolang/EOms/EOasin.java
@@ -9,6 +9,7 @@
  */
 package org.eolang.EOms; // NOPMD
 
+import org.eolang.AtVoid;
 import org.eolang.Atom;
 import org.eolang.Dataized;
 import org.eolang.PhDefault;
@@ -21,13 +22,22 @@ import org.eolang.XmirObject;
  * @since 0.40
  * @checkstyle TypeNameCheck (100 lines)
  */
-@XmirObject(oname = "real.asin")
-@SuppressWarnings("PMD.AvoidDollarSigns")
-public final class EOreal$EOasin extends PhDefault implements Atom {
+@XmirObject(oname = "asin")
+public final class EOasin extends PhDefault implements Atom {
+    /**
+     * Ctor.
+     */
+    @SuppressWarnings("PMD.ConstructorOnlyInitializesOrCallOtherConstructors")
+    public EOasin() {
+        this.add("num", new AtVoid("num"));
+    }
+
     @Override
     public Phi lambda() {
         return new ToPhi(
-            Math.asin(new Dataized(this.take(Phi.RHO)).asNumber())
+            Math.asin(
+                new Dataized(this.take("num")).asNumber()
+            )
         );
     }
 }
