91bc5dbd510317234e65d642d182b02f8aeffd4c
AlexNetTie <sanya.pletniov3196@gmail.com>
2026-04-03 21:27:35 +0300

    исправлены замечания qulice

--- eo-runtime/src/test/java/org/eolang/DataizedTest.java
+++ eo-runtime/src/test/java/org/eolang/DataizedTest.java
@@ -13,7 +13,6 @@ import java.util.logging.Logger;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
