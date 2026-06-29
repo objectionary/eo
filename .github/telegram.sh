6e64c4a236ecb61ce539ef3cd28c494f257716fb
Yegor Bugayenko <yegor256@gmail.com>
2026-06-29 12:19:27 +0300

    Update link to atoms in telegram.sh script.

--- .github/telegram.sh
+++ .github/telegram.sh
@@ -22,8 +22,8 @@ git fetch --tags --force
     "$(git rev-list --count origin/master)"
   printf '[%d objects](https://github.com/objectionary/eo/tree/master/eo-runtime/src/main/eo/org/eolang) ' \
     "$(find eo-runtime/src/main/eo -name '*.eo' | wc -l)"
-  printf 'and [%d atoms](https://github.com/objectionary/eo/tree/master/eo-runtime/src/main/java/EOorg/EOeolang) ' \
-    "$(find eo-runtime/src/main/java/EOorg -name 'EO*.java' | wc -l)"
+  printf 'and [%d atoms](https://github.com/objectionary/eo/tree/master/eo-runtime/src/main/java/org/eolang) ' \
+    "$(find eo-runtime/src/main/java/org -name 'EO*.java' | wc -l)"
   printf 'in the [eo-runtime](https://github.com/objectionary/eo/tree/master/eo-runtime) module; '
   printf '[%d issues](https://github.com/objectionary/eo/issues?q=is%%3Aopen+is%%3Aissue+label%%3A%%22help+wanted%%22) ' \
     "$(curl --silent 'https://api.github.com/repos/objectionary/eo/issues?state=open&per_page=100&labels=help%20wanted' | jq length)"
