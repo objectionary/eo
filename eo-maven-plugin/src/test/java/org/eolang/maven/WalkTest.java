1f5abe482a2b6fad99130a92183461c370ccafdc
h1alexbel <h1alexbelx@gmail.com>
2026-07-31 11:30:43 +0300

    bug(#6096): removed redundant test because of simplified semantics

--- eo-maven-plugin/src/test/java/org/eolang/maven/WalkTest.java
+++ eo-maven-plugin/src/test/java/org/eolang/maven/WalkTest.java
@@ -6,7 +6,6 @@ package org.eolang.maven;
 
 import com.yegor256.Mktmp;
 import com.yegor256.MktmpResolver;
-import java.io.IOException;
 import java.nio.file.Path;
 import java.util.regex.PatternSyntaxException;
 import org.cactoos.list.ListOf;
@@ -37,17 +36,6 @@ final class WalkTest {
             ),
             new Walk(temp).includes(new ListOf<>(pattern)),
             Matchers.iterableWithSize(count)
-        );
-    }
-
-    @Test
-    void throwsExceptionOnBadGlobs(@Mktmp final Path temp) throws IOException {
-        new Saved("", temp.resolve("foo/app/0.1/EOfoo/foo.bin")).value();
-        new Saved("", temp.resolve("EOxxx/foo")).value();
-        Assertions.assertThrows(
-            IllegalArgumentException.class,
-            () -> new Walk(temp).includes(new ListOf<>("{foo")),
-            "Exception must be thrown for invalid glob pattern"
         );
     }
 
