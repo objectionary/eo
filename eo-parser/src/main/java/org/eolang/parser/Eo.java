98d6da82f3d84eaec706519eec51f6aeb1a04d52
Titanism0v0 <15958739196@163.com>
2026-07-31 20:46:43 +0800

    fix(#6183): remove arbitrary spans capacity

--- eo-parser/src/main/java/org/eolang/parser/Eo.java
+++ eo-parser/src/main/java/org/eolang/parser/Eo.java
@@ -71,7 +71,7 @@ final class Eo implements Iterable<Directive> {
             level -> Eo.checkOnClose(level, emit),
             parent -> Eo.beforeChild(parent, emit)
         );
-        final java.util.List<Span> spans = new java.util.ArrayList<>(16);
+        final java.util.List<Span> spans = new java.util.ArrayList<>(0);
         new Source(this.source).forEach(spans::add);
         int idx = 0;
         while (idx < spans.size()) {
