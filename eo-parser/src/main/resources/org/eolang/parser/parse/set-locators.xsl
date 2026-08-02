3795ebe181a575c218a2feb724970f43d7b4d042
Titanism0v0 <15958739196@163.com>
2026-08-02 11:40:23 +0800

    #6079: guard XSL helper namespaces

--- eo-parser/src/main/resources/org/eolang/parser/parse/set-locators.xsl
+++ eo-parser/src/main/resources/org/eolang/parser/parse/set-locators.xsl
@@ -3,7 +3,7 @@
 * SPDX-FileCopyrightText: Copyright (c) 2016-2026 Objectionary.com
 * SPDX-License-Identifier: MIT
 -->
-<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:eo="https://www.eolang.org" xmlns:xs="http://www.w3.org/2001/XMLSchema" id="set-locators" version="2.0">
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:eo="https://www.eolang.org" xmlns:xs="http://www.w3.org/2001/XMLSchema" exclude-result-prefixes="eo xs" id="set-locators" version="2.0">
   <!--
   Here we go through all objects and add @loc attributes
   to all of them. The value of the attribute is a unique locator
---
+++ src/test/groovy/check-xsl-result-prefixes.groovy
@@ -0,0 +1,41 @@
+/**
+ * SPDX-FileCopyrightText: Copyright (c) 2016-2026 Objectionary.com
+ * SPDX-License-Identifier: MIT
+ */
+
+/**
+ * This check verifies that stylesheet-only namespaces are excluded
+ * from literal result elements.
+ */
+import groovy.io.FileType
+import groovy.io.FileVisitResult
+import javax.xml.parsers.DocumentBuilderFactory
+
+File project = new File('.')
+DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance()
+factory.setNamespaceAware(true)
+
+project.traverse(
+    type: FileType.FILES,
+    preDir: { file ->
+        if (file.name == 'target') {
+            return FileVisitResult.SKIP_SUBTREE
+        }
+    },
+    nameFilter: ~/.*\.xsl/,
+) {
+    file ->
+    def stylesheet = factory.newDocumentBuilder().parse(file).documentElement
+    Set<String> excluded = stylesheet
+        .getAttribute('exclude-result-prefixes')
+        .tokenize()
+        .toSet()
+    ['eo', 'xs'].each {
+        prefix ->
+        if (stylesheet.lookupNamespaceURI(prefix) != null) {
+            assert excluded.contains(prefix) || excluded.contains('#all') :
+                "${file} declares '${prefix}' but does not exclude it from results"
+        }
+    }
+}
+true
