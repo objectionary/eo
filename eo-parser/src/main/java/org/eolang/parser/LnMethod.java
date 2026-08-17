a5d1f84b11ae1235be4fae28f696c80f849e3d33
devil <europedreadlydevil@gmail.com>
2026-08-17 12:21:57 +0000

    #6314: keep the meta-header blank line readable by the test-attribute check

--- eo-parser/src/main/java/org/eolang/parser/LnMethod.java
+++ eo-parser/src/main/java/org/eolang/parser/LnMethod.java
@@ -75,7 +75,7 @@ final class LnMethod implements Line {
         );
         suffix.rejectAtomOutsideFormation(this.span);
         if (suffix.test()) {
-            Blanks.checkTest(this.span, globals, emit);
+            Blanks.checkTest(this.span, globals.pendingBlanks(), emit);
         } else {
             Blanks.checkPlain(this.span, globals, emit);
         }
