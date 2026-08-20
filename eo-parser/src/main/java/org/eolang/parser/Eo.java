fbcbbdb7d14cac3dd45dbc4b0caff9698db8c9af
devil <europedreadlydevil@gmail.com>
2026-08-20 15:28:25 +0000

    #7101: validate the closer's suffix boundary in closesTextBlock

--- eo-parser/src/main/java/org/eolang/parser/Eo.java
+++ eo-parser/src/main/java/org/eolang/parser/Eo.java
@@ -338,9 +338,11 @@ final class Eo implements Iterable<Directive> {
     }
 
     private static boolean closesTextBlock(final Span span, final Globals globals) {
+        final String body = span.body();
         return !span.blank()
             && span.indent() == globals.textBlockOpenIndent()
-            && span.body().startsWith("\"\"\"");
+            && body.startsWith("\"\"\"")
+            && " .".indexOf(body.concat(" ").charAt(3)) >= 0;
     }
 
     private static Line classify(final Span span) {
