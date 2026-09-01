558852489a1a534638dcb70d5da99cc08cad4459
devil <europedreadlydevil@gmail.com>
2026-09-01 08:30:12 +0000

    #8134: give the EO_string test package its package-info

---
+++ eo-runtime/src/test/java/org/eolang/EO_string/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2016-2026 Objectionary.com
+ * SPDX-License-Identifier: MIT
+ */
+
+/**
+ * Tests for the objects of the string package, transpiled.
+ * @since 0.61.0
+ * @checkstyle TrailingCommentCheck (3 lines)
+ */
+package org.eolang.EO_string; // NOPMD
