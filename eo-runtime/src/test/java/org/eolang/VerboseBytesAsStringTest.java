9d7919e6bbae5f45490f0e9fb2ed10ef8c9ef2f7
devil <europedreadlydevil@gmail.com>
2026-09-06 11:16:47 +0000

    #8400: call a byte a boolean only when it is one

--- eo-runtime/src/test/java/org/eolang/VerboseBytesAsStringTest.java
+++ eo-runtime/src/test/java/org/eolang/VerboseBytesAsStringTest.java
@@ -72,10 +72,11 @@ final class VerboseBytesAsStringTest {
             ),
             Arguments.of(new byte[]{-1}, "[0xFF] = true"),
             Arguments.of(new byte[]{0}, "[0x00] = false"),
-            Arguments.of(new byte[]{1}, "[0x01] = false"),
-            Arguments.of(new byte[]{2}, "[0x02] = false"),
+            Arguments.of(new byte[]{1}, "[0x01] = \"\\u0001\""),
+            Arguments.of(new byte[]{2}, "[0x02] = \"\\u0002\""),
             Arguments.of(new byte[]{}, "[<no bytes>]"),
-            Arguments.of(new byte[]{12}, "[0x0C] = false"),
+            Arguments.of(new byte[]{12}, "[0x0C] = \"\\u000c\""),
+            Arguments.of(new byte[]{0x41}, "[0x41] = \"A\""),
             Arguments.of(
                 new byte[]{0x61, 0x22, 0x62, 0x5C, 0x63, 0x7F},
                 "[0x6122625C-637F] = \"a\\\"b\\\\c\\u007f\""
