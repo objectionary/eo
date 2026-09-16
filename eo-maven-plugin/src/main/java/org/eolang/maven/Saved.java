0132c35baefa6ddcb475278763114d66726a8b8f
gemshrine <113778529+gemshrine@users.noreply.github.com>
2026-09-16 12:51:46 +0300

    fix(#8144): pad short temporary prefixes

--- eo-maven-plugin/src/main/java/org/eolang/maven/Saved.java
+++ eo-maven-plugin/src/main/java/org/eolang/maven/Saved.java
@@ -112,9 +112,11 @@ final class Saved implements Scalar<Path> {
             if (dir.toFile().mkdirs()) {
                 Logger.debug(this, "Directory created: %[file]s", dir);
             }
+            final String filename = this.target.getFileName().toString();
+            final String prefix = String.format("%-3s", filename);
             final Path tmp = Files.createTempFile(
                 dir,
-                this.target.getFileName().toString(),
+                prefix,
                 ".tmp"
             );
             try {
